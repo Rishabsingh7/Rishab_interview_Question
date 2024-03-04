90db97cfa0f7e8f13419a083e35cedc78026f194
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-03-04 19:56:45 +0000

    Add First Question

--- Interview_Question/One.js
+++ Interview_Question/One.js
@@ -1,1 +1,22 @@
-console.log("To Kaise Ho App Log");
+//Question number 1
+//Ek Array ke under kuch Additional Properties hai 
+//Ek Array hai uspe loop lagana hai uski original propeties ko display karwana hai
+Array.prototype.extraProperty="rishab"
+const myNewArray=[1,2,3,4,5,6];
+// for(let singh in myNewArray){
+//     console.log(singh);
+// }
+
+
+    //  for(let singh in myNewArray){
+    //     if(myNewArray.hasOwnProperty(singh)){
+    //         console.log(singh);
+    //     }
+    //  }
+
+    //Other ways
+    for(let i=0;i<myNewArray.length;i++){
+        if(myNewArray.hasOwnProperty(i)){
+            console.log(i);
+        }
+    }
